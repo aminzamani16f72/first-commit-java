bfd28b91e9a60678219322c0bbb041a9faa5313e
amin <aminzamani.z72@gmail.com>
2023-02-09 13:48:17 +0330

    firstc

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Addable addable = (int p1, int p2)->(p1+p2);
+        Integer add=(int a,int b)->(a+b);
+
+        Addable ad1=(int a,int b)->(a+b);
+        System.out.println(ad1.add(10,20));
+
+        List<String> list=new ArrayList<String>();
+        list.add("ankit");
+        list.add("mayank");
+        list.add("irfan");
+        list.add("jai");
+
+        list.forEach(
+                (n)->System.out.println(n+2)
+        );
+    }
+
+
+}
+interface Addable{
+    int add(int a,int b);
+}
