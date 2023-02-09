a8777b10359159dcfb28e874c643a98e084dfd25
amin <aminzamani.z72@gmail.com>
2023-02-09 14:40:15 +0330

    secommit

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         Addable addable = (int p1, int p2)->(p1+p2);
-        Integer add=(int a,int b)->(a+b);
+        
 
         Addable ad1=(int a,int b)->(a+b);
         System.out.println(ad1.add(10,20));
 
         List<String> list=new ArrayList<String>();
-        list.add("ankit");
+
         list.add("mayank");
         list.add("irfan");
         list.add("jai");
